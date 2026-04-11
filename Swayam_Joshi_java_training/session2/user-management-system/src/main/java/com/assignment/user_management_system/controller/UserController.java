8e1809f6716f56a69850277fe3a1cc00cee53277
Swayam-Joshi <swayamjoshi13@gmail.com>
2026-04-12 01:08:01 +0530

    feat: add user REST APIs (GET all, POST, create, GET by ID)

---
+++ Swayam_Joshi_java_training/session2/user-management-system/src/main/java/com/assignment/user_management_system/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.assignment.user_management_system.controller;
+
+import com.assignment.user_management_system.model.User;
+import com.assignment.user_management_system.service.UserService;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAll() { return userService.getAllUsers(); }
+
+    @PostMapping
+    public String create(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+
+    // Required "Twist" API: /users/message/1?type=SHORT
+    @GetMapping("/message/{id}")
+    public Map<String, String> getWelcomeMessage(@PathVariable Long id, @RequestParam String type) {
+        return Map.of("message", userService.getFormattedWelcome(id, type));
+    }
+}
