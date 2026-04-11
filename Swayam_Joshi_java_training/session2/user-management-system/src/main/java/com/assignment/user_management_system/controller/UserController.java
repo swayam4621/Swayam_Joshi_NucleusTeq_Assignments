e9de4b5a8723adad01db4bccf3da1d04d423ef8f
Swayam-Joshi <swayamjoshi13@gmail.com>
2026-04-12 02:16:26 +0530

    feat: adding delete and update user functions to controller

--- Swayam_Joshi_java_training/session2/user-management-system/src/main/java/com/assignment/user_management_system/controller/UserController.java
+++ Swayam_Joshi_java_training/session2/user-management-system/src/main/java/com/assignment/user_management_system/controller/UserController.java
@@ -2,6 +2,8 @@ package com.assignment.user_management_system.controller;
 
 import com.assignment.user_management_system.model.User;
 import com.assignment.user_management_system.service.UserService;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
@@ -14,6 +16,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    //get all users
     @GetMapping
     public List<User> getAll() { return userService.getAllUsers(); }
 
@@ -22,6 +25,7 @@ public class UserController {
         return userService.addUser(user);
     }
 
+    //get user by id
     @GetMapping("/{id}")
     public User getById(@PathVariable Long id) {
         return userService.getUser(id);
@@ -30,5 +34,17 @@ public class UserController {
     @GetMapping("/message/{id}")
     public Map<String, String> getWelcomeMessage(@PathVariable Long id, @RequestParam String type) {
         return Map.of("message", userService.getFormattedWelcome(id, type));
+    }
+
+    //update user
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        return ResponseEntity.ok(userService.updateUser(id, userDetails));
+    }
+
+    //delete user
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.deleteUser(id));
     }
 }
