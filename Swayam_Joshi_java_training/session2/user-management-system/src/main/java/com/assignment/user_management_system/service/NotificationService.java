30cdca1a8df08d714167bb6f500857db99bf7191
Swayam-Joshi <swayamjoshi13@gmail.com>
2026-04-12 14:55:38 +0530

    fix: remove the Map<String, MessageFormatter> from this file. It doesn't need to know about formatting anymore and other fixes

--- Swayam_Joshi_java_training/session2/user-management-system/src/main/java/com/assignment/user_management_system/service/NotificationService.java
+++ Swayam_Joshi_java_training/session2/user-management-system/src/main/java/com/assignment/user_management_system/service/NotificationService.java
@@ -1,9 +1,8 @@
 package com.assignment.user_management_system.service;
 
 import com.assignment.user_management_system.component.NotificationComponent;
-import com.assignment.user_management_system.formatter.MessageFormatter;
 import org.springframework.stereotype.Service;
-import java.util.Map;
+
 
 @Service
 public class NotificationService {
@@ -17,16 +16,5 @@ public class NotificationService {
     // Task: Service calls a NotificationComponent
     public String sendSystemNotification() {
         return notificationComponent.send();
-    }
-
-    // Task: Dynamic Message Formatter logic (No if-else)
-    public String getFormattedMessage(String type, String content) {
-        MessageFormatter formatter = formatters.get(type.toUpperCase());
-        
-        if (formatter == null) {
-            throw new IllegalArgumentException("Invalid format type: " + type);
-        }
-        
-        return formatter.format(content);
     }
 }
