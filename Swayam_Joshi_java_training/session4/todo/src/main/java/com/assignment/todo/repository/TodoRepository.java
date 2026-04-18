104203e29c1faa791eec36270c0838ea180c0168
Swayam-Joshi <swayamjoshi13@gmail.com>
2026-04-19 01:49:49 +0530

    feat: implement TodoRepository for data access

---
+++ Swayam_Joshi_java_training/session4/todo/src/main/java/com/assignment/todo/repository/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.assignment.todo.repository;
+
+import com.assignment.todo.model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+    // JPA repository provides all required CRUD methhods automatically
+}
