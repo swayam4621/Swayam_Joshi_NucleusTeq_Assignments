8bdd420bb360eb8112968178c961495760f03e1e
Swayam-Joshi <swayamjoshi13@gmail.com>
2026-06-29 13:16:45 +0530

    chore: initial backend scaffolding and dependencies

---
+++ Capstone_CircleUp/backend/app/models/__init__.py
@@ -0,0 +1,3 @@
+from app.models.user import User  # noqa: F401
+from app.models.activity import Activity, ActivityStatus  # noqa: F401
+from app.models.activity_participation import ParticipationRequest, ParticipationStatus  # noqa: F401
